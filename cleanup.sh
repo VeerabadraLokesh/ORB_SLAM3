ca47da461876527f45e4c2eb0e1eef8c6ea44fbf
Lokesh Paturu <lokesh.pvl@gmail.com>
2023-08-16 16:39:48 -0400

    cleanup

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+echo "removing some files"
+
+rm CMakeLists.txt Calibration_Tutorial.pdf Changelog.md Dependencies.md LICENSE README.md build_ros.sh build.sh
+rm -rf Examples_old evaluation
+
+rm -rf Examples/{Calibration,Monocular-Inertial,REAMDME.md,RGB-D,RGB-D-Inertial,Stereo,Stereo-Inertial}
+
+rm -rf Examples/Monocular/{EuRoC_TimeStamps,TUM_TimeStamps}
+
+# Thirdparty
+# Vocabulary
+# build
+# include
+# lib
+# src
+
+# Examples
