13dcf8dc92cb158f1c830753a2140595d6d88170
Lokesh Paturu <lokesh.pvl@gmail.com>
2023-08-16 21:39:04 -0400

    log

--- cleanup.sh
+++ cleanup.sh
@@ -6,10 +6,13 @@ cd /opt/docker/ORB_SLAM3
 rm -f CMakeLists.txt Calibration_Tutorial.pdf Changelog.md Dependencies.md LICENSE README.md build_ros.sh build.sh
 rm -rf Examples_old/ evaluation/
 
-rm -rf Examples/{Calibration,Monocular-Inertial,REAMDME.md,RGB-D,RGB-D-Inertial,Stereo,Stereo-Inertial}
+cd Examples/
+rm -rf Calibration/ Monocular-Inertial/ REAMDME.md RGB-D/ RGB-D-Inertial/ Stereo/ Stereo-Inertial/
 
-rm -rf Examples/Monocular/{EuRoC_TimeStamps,TUM_TimeStamps}
+cd Monocular/
+rm -rf EuRoC_TimeStamps TUM_TimeStamps
 
+cd ../..
 rm -f Vocabulary/ORBvoc.txt.tar.gz
 
 ls -ltr
