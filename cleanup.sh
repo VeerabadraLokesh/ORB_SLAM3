897cb5a76e67928b593ce0c0b6082170556a34e2
Lokesh Paturu <lokesh.pvl@gmail.com>
2023-08-16 21:10:42 -0400

    log

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,23 @@
 echo "removing some files"
 
+du -sh /opt/docker/ORB_SLAM3
+cd /opt/docker/ORB_SLAM3
+
 rm -f CMakeLists.txt Calibration_Tutorial.pdf Changelog.md Dependencies.md LICENSE README.md build_ros.sh build.sh
-rm -rf Examples_old evaluation
+rm -rf Examples_old/ evaluation/
 
 rm -rf Examples/{Calibration,Monocular-Inertial,REAMDME.md,RGB-D,RGB-D-Inertial,Stereo,Stereo-Inertial}
 
 rm -rf Examples/Monocular/{EuRoC_TimeStamps,TUM_TimeStamps}
 
 rm -f Vocabulary/ORBvoc.txt.tar.gz
+
+ls -ltr
+
+cd /opt/docker
+du -sh *
+
+eco "done"
 
 # Thirdparty
 # Vocabulary
